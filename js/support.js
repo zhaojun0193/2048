0ecf353bf4602b14b859837340e71dc59a1d9003
tiakon <1036615650@qq.com>
2017-07-08 11:29:12 +0800

    游戏基本样式

--- js/support.js
+++ js/support.js
@@ -1,6 +1,11 @@
 /**
- * Created by Hoictas on 2017/7/5.
-
- hhhhhhhhhhhh
+ * Created by Tiakon on 2017/7/5.
  */
-
+//通过getTop()方法设置每个格子距顶端的距离
+function getTop(j) {
+    return 20+j*120;
+}
+//通过getLeft()方法设置每个格子距左端的距离
+function getLeft(i) {
+    return 20+i*120;
+}
